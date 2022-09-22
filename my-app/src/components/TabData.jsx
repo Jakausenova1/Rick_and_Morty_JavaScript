30eef2f9b20c8b8c762e73e0258157e2b3a81db7
Jaka Usenova <97898976+Jakausenova1@users.noreply.github.com>
2022-09-23 01:40:35 +0600

    логика

--- my-app/src/components/TabData.jsx
+++ my-app/src/components/TabData.jsx
@@ -1,46 +1,59 @@
-import React, { useEffect } from "react";
-import {
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Table,
-} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Container, Box, TextField } from "@mui/material";
 
 const TabData = () => {
+  const [user, setUser] = useState({});
+  const [episode, setEpisode] = useState("");
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character/1")
+      .then((response) => response.json())
+      .then((data) => setUser(data));
+  }, []);
+
+  const fetchData = () => {
+    fetch(`https://rickandmortyapi.com/api/character/${episode}`)
+      .then((response) => response.json())
+      .then((data) => setUser(data));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();  //отменяет действие браузера по умолчанию 
+    setEpisode(e.target.value);
+    fetchData();
+    setEpisode("");
+  };
+  console.log(episode);
+
   return (
-    <>
-      <TableContainer component={Paper}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell align="right">Photo</TableCell>
-              <TableCell align="right">Episode</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell component="th" scope="row">
-                s
-              </TableCell>
-              <TableCell align="right">sss</TableCell>
-              <TableCell align="right">ss</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
+    <Container>
+      <Container sx={{ display: "flex", gap: "100px" }}>
+        <Box>
+          <h4>Name</h4> <Box>{user.name}</Box>
+        </Box>
+        <Box>
+          <h4>Photo</h4>
+          <Box>
+            <img src={user.image} />
+          </Box>
+        </Box>
+        <Box>
+          <h4>Episode</h4> <Box></Box>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              type="number"
+              id="message"
+              name="message"
+              label="episode"
+              value={episode}
+              required
+              onChange={(e) => setEpisode(e.target.value)}
+            />
+          </form>
+        </Box>
+      </Container>
+    </Container>
   );
 };
 
 export default TabData;
-
-
-  // useEffect(() => {
-  //   fetch("http..")
-  //     .then((response) => response.json())
-  //     .then((data) => console.log(data));
-  // }, []);
